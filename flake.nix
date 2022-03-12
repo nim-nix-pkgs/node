23aec6750e0a5b512d7bf7fb255b0493c4fb7207
hugosenari <hugosenari@gmail.com>
2022-03-12 04:34:00 -0300

    chore: re index node

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Library for async programming and communication. This Library uses a future/promise, non-blocking I/O model based on libuv.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."node-master".url = "path:./master";
+  inputs."node-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."node-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
